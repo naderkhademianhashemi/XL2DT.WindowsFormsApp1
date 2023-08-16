3958ac5380907496af616f596e3aa38d32a19959
nader <nhashemi6@gmail.com>
2023-08-16 15:55:47 +0330

    Add project files.

---
+++ XL2DT.WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace XL2DT.WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrws = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(139, 61);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(446, 22);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrws
+            // 
+            this.btnBrws.Location = new System.Drawing.Point(139, 118);
+            this.btnBrws.Name = "btnBrws";
+            this.btnBrws.Size = new System.Drawing.Size(75, 23);
+            this.btnBrws.TabIndex = 1;
+            this.btnBrws.Text = "...";
+            this.btnBrws.UseVisualStyleBackColor = true;
+            this.btnBrws.Click += new System.EventHandler(this.btnBrws_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "path";
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(242, 118);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 3;
+            this.btnCount.Text = "row count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrws);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrws;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCount;
+    }
+}
+
---
+++ XL2DT.WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XL2DT.WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DataTable XlS2DT(string path)
+        {
+            var nameSHT = "Sheet1";
+            var strCN = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path +
+                            ";Extended Properties='Excel 8.0;HDR=YES;';";
+            strCN = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path +
+                ";Extended Properties=Excel 12.0 Xml;HDR=YES";
+
+            var CN = new OleDbConnection(strCN);
+            var CMD = new OleDbCommand("Select * From [" + nameSHT + "$]", CN);
+            var ADP = new OleDbDataAdapter(CMD);
+            var DT = new DataTable();
+            ADP.Fill(DT);
+            return DT;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnCount.Enabled = false;
+
+        }
+
+        private void btnBrws_Click(object sender, EventArgs e)
+        {
+            txtPath.Text= GetPath();
+            btnCount.Enabled= true;
+        }
+
+        private string GetPath()
+        {
+            return "D:\\Desktop-1401\\Excel Northwind\\2.xlsx";
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            DataTable DT = new DataTable();
+            DT = XlS2DT(txtPath.Text);
+            MessageBox.Show(DT.Rows.Count.ToString());
+        }
+
+        
+    }
+}
